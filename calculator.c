5cadb3f6368fec0710a96b8e80be5b3d12ac7942
dexnoj <wgxcm673@gmail.com>
2026-09-04 22:46:20 +0400

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,14 @@
+<#include studio.h>
+int main(){
+    double a; b; n; char e;
+    printf("Eded1 emel eded2:  ");
+    scanf("%lf %c %lf", &a, &e, &b);
+    if (e == '+') n = a + b;
+    else if (e == '-') n = a - b;
+    else if (e == '*') n = a * b;
+    else n = a / b;
+    printf("Netice %.2lf\n", n);
+    return 0;
+}
+
+
