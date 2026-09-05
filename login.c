0746a3f4f7fe14d13e88f9ab8a83d12671fa2eed
dexnoj <wgxcm673@gmail.com>
2026-09-05 13:41:47 +0400

    Create login.c

---
+++ login.c
@@ -0,0 +1,9 @@
+#include <studio>
+int main(){
+    printf("<DOCTYPE html>\n");
+    printf("<html lang='en'>\n<head>\n")
+    printf("<meta charset='UTF-8>\n");
+    printf("<title>LOGIN dealOS<title>\n");
+    printf("<style>\n")
+    printf("body{font-family:Arial;backgraund;#1e1e2f;colour:white;");
+    printf("display:flex;justify
