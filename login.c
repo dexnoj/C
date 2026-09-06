16615516c7a2664468e5554f3fc514aa2b7697fa
dexnoj <wgxcm673@gmail.com>
2026-09-06 16:01:41 +0400

    Delete login.c

--- login.c
+++
@@ -1,9 +0,0 @@
-#include <studio>
-int main(){
-    printf("<DOCTYPE html>\n");
-    printf("<html lang='en'>\n<head>\n")
-    printf("<meta charset='UTF-8>\n");
-    printf("<title>LOGIN dealOS<title>\n");
-    printf("<style>\n")
-    printf("body{font-family:Arial;backgraund;#1e1e2f;colour:white;");
-    printf("display:flex;justify
